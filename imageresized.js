2c5141d985ec64fc34b489a229dab159ea1122de
Loic Calvy <lcalvy@gmail.com>
2013-09-19 18:04:31 +0200

    optimization jQuery detection

--- imageresized.js
+++ imageresized.js
@@ -1,31 +1,45 @@
-var s = document.createElement('script');
-s.src = "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
+if (typeof jQuery=='undefined') { // typeof jQuery=='undefined' works too  
 
-if(s.addEventListener) {
-    s.readyState = "complete";
-  s.addEventListener("load",callback,false);
-} 
-else if(s.readyState) {
-  s.onreadystatechange = callback;
+    var s = document.createElement('script');
+    s.src = "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
+
+    if(s.addEventListener) {
+        s.readyState = "complete";
+      s.addEventListener("load",callback,false);
+  }
+  else if(s.readyState) {
+      s.onreadystatechange = callback;
+    }
+
+
+
+    document.body.appendChild(s);
+
+
+
 }
-function callback () {
-        alert(s.readyState);
-        var state = s.readyState;
+else{
+    checkImageSizes();
+}
 
-        if (!state || /loaded|complete/.test(state)) {
-            jQuery.noConflict();
-     
+function callback(){
+var state = s.readyState;
+    if (!state || /loaded|complete/.test(state)) {
+        jQuery.noConflict();
+    }
+    checkImageSizes();
+
+}
+
+function checkImageSizes () {
         jQuery('img').each(function(i,el){
-        console.log(el.clientWidth, el.naturalWidth); 
+        if(el.naturalWidth > el.clientWidth || el.naturalHeight > el.clientHeight){
+        //console.log(el.clientWidth, el.naturalWidth); 
         el.style.borderStyle="solid";
         el.style.borderWidth="5px";
         el.style.borderColor="red";
-        $(el).after("<span style='position: relative;left: 5px;top: -"+el.clientHeight+"px;padding: 0.5em 20px;width: 460px;background: rgba(0,0,0,0.8);text-decoration: none !important;color: #fff;opacity: 0.8;-webkit-transition: 0.5s;-moz-transition: 0.5s;-o-transition: 0.5s;-ms-transition: 0.5s;'>"+el.clientWidth+"*"+el.clientHeight+"&lt;&gt;"+el.naturalWidth+"*"+el.naturalHeight+"</span>");
-        });
-        }
-
-    
-};
-
-
-document.body.appendChild(s);
+        jQuery(el).after("<span style='position: relative;left: 5px;top: -"+el.clientHeight+"px;padding: 0.5em 20px;width: 460px;background: rgba(0,0,0,0.8);text-decoration: none !important;color: #fff;opacity: 0.8;-webkit-transition: 0.5s;-moz-transition: 0.5s;-o-transition: 0.5s;-ms-transition: 0.5s;'>"+el.clientWidth+"x"+el.clientHeight+" &lt; "+el.naturalWidth+"x"+el.naturalHeight+"</span>");
+        }});
+        
+        
+}
