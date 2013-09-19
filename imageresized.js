d03ce0655cecede3912377287fb2b59aa7931633
Loic Calvy <lcalvy@gmail.com>
2013-09-19 21:38:44 +0200

    clean image information if already processed

--- imageresized.js
+++ imageresized.js
@@ -32,13 +32,14 @@ var state = s.readyState;
 }
 
 function checkImageSizes () {
+        jQuery('.bookmarklet-imageresized').remove();
         jQuery('img').each(function(i,el){
         if(el.naturalWidth > el.clientWidth || el.naturalHeight > el.clientHeight){
         //console.log(el.clientWidth, el.naturalWidth); 
         el.style.borderStyle="solid";
         el.style.borderWidth="5px";
         el.style.borderColor="red";
-        jQuery(el).after("<span style='z-index:100;position: relative;left: 5px;top: -"+el.clientHeight+"px;padding: 0.5em 20px;width: 460px;background: rgba(0,0,0,0.8);text-decoration: none !important;color: #fff;opacity: 0.8;-webkit-transition: 0.5s;-moz-transition: 0.5s;-o-transition: 0.5s;-ms-transition: 0.5s;'>"+el.clientWidth+"x"+el.clientHeight+" &lt; "+el.naturalWidth+"x"+el.naturalHeight+"</span>");
+        jQuery(el).after("<span class='bookmarklet-imageresized' style='z-index:100;position: relative;left: 5px;top: -"+el.clientHeight+"px;padding: 0.5em 20px;width: 460px;background: rgba(0,0,0,0.8);text-decoration: none !important;color: #fff;opacity: 0.8;-webkit-transition: 0.5s;-moz-transition: 0.5s;-o-transition: 0.5s;-ms-transition: 0.5s;'>"+el.clientWidth+"x"+el.clientHeight+" &lt; "+el.naturalWidth+"x"+el.naturalHeight+"</span>");
         }});
         
         
